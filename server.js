a235df1e27539a8d956102c1c24dc2a248215ac4
Yachu <yachu@Yachude-MacBook-Pro.local>
2017-07-06 17:12:00 +0800

    123

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/apple-pay/pay', (req, res) => {
 // app.get('/ccc',function(req,res){
 //   res.send('ccccc');
 // })
-  app.listen( process.env.PORT || 3000, function(){
+  app.listen( 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     //console.log("hihihhi");
 });
